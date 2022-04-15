42a404df2e2c039a24731f9adb48953f3922290d
JakeAcosta <jacosta1913@yahoo.com>
2022-04-14 22:52:05 -0700

    Modified 1

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-//Your Code here
+let firstAnswer  = window.prompt('Do you head left or right?')
+if(firstAnswer === 'left')
+
+{let secondAnswer = window.prompt(`You come across a stray cat. 
+It scampers off down a small hole, just large enough for you to crawl through. 
+Do you follow it, or continue on your path?`)
+
+if(secondAnswer === 'follow') 
+
+{
+let secondAnswer = window.prompt('You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical safe haven.')
+}}
+
+let thirdAnswer = window.prompt('Do you stay or spread the word?')
+if(thirdAnswer === 'stay')
+
+{let fourthAnswer = window.prompt('You live happily ever after!')
+
+if(fourthAnswer === 'spread the word')
+
+{
+let fourthAnswer = window.prompt('After leaving the cat colony, you are never able to find it again; without proof, no one believes your story, which passes into legend nonetheless.')
+}}
